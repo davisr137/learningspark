062ddebcae08ad201aed47181b128db53f184b6d
davisr137 <davisr137@gmail.com>
2019-03-12 13:48:30 -0700

    unit tests

--- python/test/test_quick_start.py
+++ python/test/test_quick_start.py
@@ -1,4 +1,5 @@
 import unittest2 as unittest
+import pandas as pd
 
 import python.quick_start as qs
 
@@ -12,11 +13,23 @@ class TestQuickStart(unittest.TestCase):
         Read README.md file.
         """ 
         cls.textFile = qs.read_readme_file()
-    
+
     def test_count(self):
         """
         Test row count.
         """
         self.assertEqual(self.textFile.count(), 105)
+    
+    def test_lines_with_spark(self):
+        """
+        Check how many lines contain 'Spark'.
+        """
+        linesWithSpark = self.textFile.filter(self.textFile.value.contains("Spark")) 
+        self.assertEqual(linesWithSpark.count(), 20)
 
-
+    def test_to_pandas(self):
+        """
+        Test toPandas() function.
+        """
+        df = self.textFile.toPandas()
+        self.assertIsInstance(df, pd.DataFrame)
