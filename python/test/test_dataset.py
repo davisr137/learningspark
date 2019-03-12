a5bec29865e7a4bc825094b39ca387c8083b3d50
davisr137 <davisr137@gmail.com>
2019-03-12 15:10:53 -0700

    test size function

--- python/test/test_dataset.py
+++ python/test/test_dataset.py
@@ -1,5 +1,5 @@
 import unittest2 as unittest
-import config
+from config import spark
 from pyspark.sql.functions import *
 
 class TestDataset(unittest.TestCase):
@@ -11,10 +11,19 @@ class TestDataset(unittest.TestCase):
         Test 'split' function.
         """
         # Split on numeric characters
-        df = config.spark.createDataFrame([('abc127def',)], ['s',])
+        df = spark.createDataFrame([('abc127def',)], ['s',])
         words = df.select(split(df.s, '[0-9]+').alias('s')).collect()[0]
         self.assertEqual(words.s, ['abc', 'def'])
         # Split on spaces
-        df = config.spark.createDataFrame([('the quick brown fox',)], ['s',])
+        df = spark.createDataFrame([('the quick brown fox',)], ['s',])
         words = df.select(split(df.s, '\s+').alias('s')).collect()[0]
         self.assertEqual(words.s, ['the', 'quick', 'brown', 'fox'])
+    
+    def test_size(self):
+        """
+        Test 'size' function.
+        """
+        df = spark.createDataFrame([([1, 2],),([1],),([7, 8],)], ['data'])
+        rows = df.select(size(df.data).name("Len")).collect()
+        rl = [r.Len for r in rows]
+        self.assertEqual(rl, [2, 1, 2])
