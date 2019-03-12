36776ab94e67987d73018a5531bc8e3a4827580e
davisr137 <davisr137@gmail.com>
2019-03-12 14:44:33 -0700

    test split

--- python/test/test_dataset.py
+++ python/test/test_dataset.py
@@ -10,7 +10,11 @@ class TestDataset(unittest.TestCase):
         """
         Test 'split' function.
         """
+        # Split on numeric characters
         df = config.spark.createDataFrame([('abc127def',)], ['s',])
-        row = df.select(split(df.s, '[0-9]+').alias('s')).collect()
-        row = row[0]
-        self.assertEqual(row.s, ['abc', 'def'])
+        words = df.select(split(df.s, '[0-9]+').alias('s')).collect()[0]
+        self.assertEqual(words.s, ['abc', 'def'])
+        # Split on spaces
+        df = config.spark.createDataFrame([('the quick brown fox',)], ['s',])
+        words = df.select(split(df.s, '\s+').alias('s')).collect()[0]
+        self.assertEqual(words.s, ['the', 'quick', 'brown', 'fox'])
