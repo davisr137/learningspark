9a582674fc6e8f003831fccb48658a4fa35834e4
davisr137 <davisr137@gmail.com>
2019-03-13 09:52:10 -0700

    test explode and groupBy

--- python/test/test_dataset.py
+++ python/test/test_dataset.py
@@ -1,5 +1,7 @@
 import unittest2 as unittest
+import pandas as pd
 from config import spark
+from pyspark.sql import Row
 from pyspark.sql.functions import *
 
 class TestDataset(unittest.TestCase):
@@ -27,3 +29,21 @@ class TestDataset(unittest.TestCase):
         rows = df.select(size(df.data).name("Len")).collect()
         rl = [r.Len for r in rows]
         self.assertEqual(rl, [2, 1, 2])
+
+    def test_explode(self):
+        """
+        Return a new row for each element in a given array or map.
+        """
+        df = spark.createDataFrame([Row(a=1, intlist=[1,5,17], mapfield={"a": "b"})])
+        l = df.select(explode(df.intlist).alias("anInt")).collect()
+        values = [row.anInt for row in l]
+        self.assertEqual(values, [1,5,17])
+
+    def test_groupBy(self):
+        """
+        Count instances of word.
+        """
+        df = spark.createDataFrame([('hello',),('bonjour',),('hello',)], ['word'])
+        df_ct = df.groupBy("word").count().toPandas()
+        df_expected = pd.DataFrame(index=[0, 1], columns=['word', 'count'], data=[['hello', 2], ['bonjour', 1]])
+        pd.testing.assert_frame_equal(df_ct, df_expected)
